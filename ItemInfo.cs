dd80b14e063d9a4670b20bdfd5e36e80eba1d253
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-11 02:02:09 +0700

    Bump version

--- ItemInfo.cs
+++ ItemInfo.cs
@@ -39,7 +39,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "ItemInfo";
     public override string Author { get; init; } = "ODT";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.3");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.4");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
