eb69a215c1c0c7081862faa1f78ba3a6ba5f2aa4
Kobe Thuy <thuynguyentrungdang@gmail.com>
2025-11-10 17:51:25 +0700

    Bumped version Target SPT 4.0.4

--- ItemInfo.cs
+++ ItemInfo.cs
@@ -39,8 +39,8 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "ItemInfo";
     public override string Author { get; init; } = "ODT";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.4");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
     public override string? Url { get; init; }
